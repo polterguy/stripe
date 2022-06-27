1dc79513e9df6e7dbcf42782ebe21ffe86e5c6b1
Aista, Ltd <th@aista.com>
2022-06-27 21:58:05 +0300

    Bettering

--- magic.startup/stripe.mysql.sql
+++ magic.startup/stripe.mysql.sql
@@ -7,6 +7,16 @@
  */
 create table customers (
   username varchar(256) primary key,
-  customer_id varchar(256) not null,
-  payment_method varchar(256)
+  customer_id varchar(256) not null
+);
+
+/*
+ * Payment method table containing references to payment methods at Stripe.
+ */
+create table payment_methods (
+  payment_method varchar(256) primary key,
+  username varchar(256) not null,
+  card_no varchar(4) not null,
+  card_type varchar(100) not null,
+  constraint payment_methods_customers_fky foreign key (username) references customers (username)
 );
--- magic.startup/stripe.sqlite.sql
+++ magic.startup/stripe.sqlite.sql
@@ -9,6 +9,16 @@ PRAGMA foreign_keys = ON;
  */
 create table customers (
   username varchar(256) primary key,
-  customer_id varchar(256) not null,
-  payment_method varchar(256)
+  customer_id varchar(256) not null
+);
+
+/*
+ * Payment method table containing references to payment methods at Stripe.
+ */
+create table payment_methods (
+  payment_method varchar(256) primary key,
+  username varchar(256) not null,
+  card_no varchar(4) not null,
+  card_type varchar(100) not null,
+  foreign key (username) references customers (username)
 );
